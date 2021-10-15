52fcf6b8ae98c60eb11a9689ea4dc812ae7b8d25
Tomas Green <tomas.green@gmail.com>
2021-10-15 16:03:06 +0200

    Mashie tests

--- Tests/MealsTests/MealsTests.swift
+++ Tests/MealsTests/MealsTests.swift
@@ -6,7 +6,6 @@ var cancellables = Set<AnyCancellable>()
 final class MealsTests: XCTestCase {
     func testSkolmatenFirstSchool() {
         let expectation = XCTestExpectation(description: "Fetch schools")
-        
         Skolmaten.first(county: "skåne", municipality: "helsingborg", school: "råå").sink { compl in
             switch compl {
             case .failure(let error):
@@ -18,6 +17,42 @@ final class MealsTests: XCTestCase {
             }
         } receiveValue: { school in
             XCTAssert(school.title.contains("Råå förskola"))
+            expectation.fulfill()
+        }.store(in: &cancellables)
+        wait(for: [expectation], timeout: 20.0)
+    }
+    func testMashie() {
+        let organization = "cb776b5e"
+        let url = URL(string: "https://mpi.mashie.com/public/menu/helsingborg+vof/\(organization)?country=se")!
+        
+        let parameters:[MashieEaterie.Parameter] = [
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens middag", tags:[]),
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens middag mos", tags:[.easyToChew]),
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens alternativ", tags:[.alternative]),
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens alternativ mos", tags:[.alternative,.easyToChew]),
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens gröna", tags:[.vegetarian]),
+            .init(occation:.lunch,  foodType: .undecided,   title: "Dagens gröna mos", tags:[.vegetarian,.easyToChew]),
+            .init(occation:.lunch,  foodType: .dessert,     title: "Dessert"),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällsmat", tags:[]),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällsmat mos", tags:[.easyToChew]),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällsmat avvikelse veg", tags:[.vegetarian]),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällsmat avvikelse veg mos", tags:[.vegetarian,.easyToChew]),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällens enkla", tags:[.simple]),
+            .init(occation:.dinner, foodType: .undecided,   title: "Kvällens enkla mos", tags:[.simple,.easyToChew])
+        ]
+        let expectation = XCTestExpectation(description: "testMashie")
+        let mashie = MashieEaterie(url: url, orgId: organization, parameters: parameters)
+        mashie.fetchMealsPublisher().sink { compl in
+            switch compl {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+                debugPrint(error)
+                expectation.fulfill()
+            case .finished:
+                debugPrint("finished resources")
+            }
+        } receiveValue: { meals in
+            XCTAssert(meals.count > 0)
             expectation.fulfill()
         }.store(in: &cancellables)
         wait(for: [expectation], timeout: 20.0)
