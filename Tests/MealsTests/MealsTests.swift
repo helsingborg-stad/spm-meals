0ccb1cfd68fc172da6dea1b90486918c5b1e7db0
Tomas Green <tomas.green@gmail.com>
2021-10-15 11:45:29 +0200

    Added test for skolmaten

--- Tests/MealsTests/MealsTests.swift
+++ Tests/MealsTests/MealsTests.swift
@@ -1,11 +1,25 @@
-    import XCTest
-    @testable import Meals
+import XCTest
+@testable import Meals
+import Combine
 
-    final class MealsTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            //XCTAssertEqual(Meals().text, "Hello, World!")
-        }
+var cancellables = Set<AnyCancellable>()
+final class MealsTests: XCTestCase {
+    func testSkolmatenFirstSchool() {
+        let expectation = XCTestExpectation(description: "Fetch schools")
+        
+        Skolmaten.first(county: "skåne", municipality: "helsingborg", school: "råå").sink { compl in
+            switch compl {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+                debugPrint(error)
+                expectation.fulfill()
+            case .finished:
+                debugPrint("finished resources")
+            }
+        } receiveValue: { school in
+            XCTAssert(school.title.contains("Råå förskola"))
+            expectation.fulfill()
+        }.store(in: &cancellables)
+        wait(for: [expectation], timeout: 20.0)
     }
+}
