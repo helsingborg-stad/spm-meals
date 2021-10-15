49f7b96f9f2cc651bd64ac86b02606d53a344797
Tomas Green <tomas.green@gmail.com>
2021-10-15 16:03:16 +0200

    Added code documentation

--- Sources/Meals/Mashie/Mashie.swift
+++ Sources/Meals/Mashie/Mashie.swift
@@ -2,21 +2,44 @@ import Foundation
 import Combine
 import SwiftSoup
 
+/// MashieEaterie is a library for fetching meal information from the Mashie. The class implements `MealService` and can be used together with the `Meals` library.
+/// - Note: This is a fraglile implementation. Hopefully it can be completely rewritten once Mashie decides to supply a service-API.
 public struct MashieEaterie : MealService {
+    /// MashieEaterie errors
     enum MashieEaterieError : Error {
+        /// Download string decoding failure
         case dataFailure(URL)
     }
-    struct Info : Codable {
+    
+    /// Temporary object that decodes meal information
+    struct Info: Codable {
+        /// Meal score/rating
         let score:Double?
+        /// Meal climate impact
         let kgCo2E:Double?
+        /// Meal image
         let imageUrl:URL?
     }
+    
+    /// MashieEaterie parsing parameter, used when scraping the webiste for meal information
     public struct Parameter {
+        /// Meal occatyion (not used for html comparison)
         public let occation:Meal.Occasion
+        /// Type of food (not used for html comparison)
         public let foodType:Meal.FoodType
+        /// Meal tags (not used for html comparison)
         public let tags:Set<Meal.Tag>
+        /// Title of meal used to compare with html content
         public let title:String
+        /// Misc meal information (not used for html comparison)
         public let info:[String]
+        /// Initializes a new Parameter
+        /// - Parameters:
+        ///   - occation: Meal occatyion (not used for html comparison)
+        ///   - foodType: Type of food (not used for html comparison)
+        ///   - title: Meal tags (not used for html comparison)
+        ///   - tags: Title of meal used to compare with html content
+        ///   - info: Misc meal information (not used for html comparison)
         public init(occation:Meal.Occasion, foodType:Meal.FoodType = .undecided, title:String, tags:Set<Meal.Tag> = [],info:[String] = []) {
             self.occation = occation
             self.foodType = foodType
@@ -25,16 +48,28 @@ public struct MashieEaterie : MealService {
             self.info = info
         }
     }
+    /// Parameters used when scraping the website
     public let parameters:[Parameter]
+    /// Target url, exmaple https://mpi.mashie.com/public/menu/.....
     public let url:URL
+    /// The organization id, can be found in the mpi url.
     public let orgId:String
+    /// Wether or not to fetch rating, climate impact and meal image.
     public let fetchInfo:Bool
+    /// Instantiates a new MashieEaterie object.
+    /// - Parameters:
+    ///   - url: Target url, exmaple https://mpi.mashie.com/public/menu/.....
+    ///   - The organization id, can be found in the mpi url.
+    ///   - parameters: Parameters used when scraping the website, see parameter documentation for more information
+    ///   - fetchInfo: Wether or not to fetch rating, climate impact and meal image. This feature makes a separate request for each meal, exluede if you don't plan on using the information.
     public init(url:URL, orgId:String, parameters:[Parameter], fetchInfo:Bool = false) {
         self.url = url
         self.orgId = orgId
         self.parameters = parameters
         self.fetchInfo = true
     }
+    /// Publisher that fetches meast using the given parameters
+    /// - Returns: a fetch publiher
     public func fetchMealsPublisher() -> AnyPublisher<[Meal], Error> {
         let s = PassthroughSubject<[Meal],Error>()
         DispatchQueue.global().async {
